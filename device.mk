2770c0b40fc3ae5f440596fbb3049bbf8c341cdb
ryukftw <workforbijoy9@gmail.com>
2025-01-23 08:15:33 +0000

    RMX3371: Build BCR
    Signed-off-by: ryukftw <workforbijoy9@gmail.com>

--- device.mk
+++ device.mk
@@ -85,6 +85,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# BCR
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
 # Bluetooth
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
